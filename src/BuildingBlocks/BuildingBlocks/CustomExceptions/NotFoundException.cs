d06bb921cfeb4bf300abd312111eac30e4b621b0
JACEK SMART <maciej.dyminski02@gmail.com>
2025-01-05 20:05:18 +0100

    implement custom excepitons

---
+++ src/BuildingBlocks/BuildingBlocks/CustomExceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.CustomExceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string name, object key) 
+            : base($"Entity \"{name}\" ({key}) has been not found") { }
+    }
+}
