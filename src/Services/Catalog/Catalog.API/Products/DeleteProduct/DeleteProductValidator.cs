5b7abc841dcfcf9a6598e4898876b7a7cfade2a4
JACEK SMART <maciej.dyminski02@gmail.com>
2025-01-05 18:57:47 +0100

    DeleteProductValidator implementation

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    public class DeleteProductValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+        }
+    }
+}
