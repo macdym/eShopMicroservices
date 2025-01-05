7240034bc13dea10dcb529c1bd1f966675351dae
JACEK SMART <maciej.dyminski02@gmail.com>
2025-01-05 19:00:10 +0100

    UpdateProductValidator implementation

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog.API.Products.UpdateProduct
+{
+    public class UpdateProductValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is not specified.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
+}
