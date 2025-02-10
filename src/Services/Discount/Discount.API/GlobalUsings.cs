15a4c54492caee62925101373b5dfcf3747447a8
JACEK SMART <maciej.dyminski02@gmail.com>
2025-02-10 22:29:21 +0100

    fix grpc version for non asci enironment localizations

--- src/Services/Discount/Discount.API/GlobalUsings.cs
+++ src/Services/Discount/Discount.API/GlobalUsings.cs
@@ -1,2 +1,6 @@
-﻿global using BuildingBlocks.Consts;
+﻿
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using BuildingBlocks.Consts;
 global using BuildingBlocks.CustomExceptions.Handlers;
