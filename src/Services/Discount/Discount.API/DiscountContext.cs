e077205e8340b7c8e51e114d222974b316f47e42
JACEK SMART <maciej.dyminski02@gmail.com>
2025-02-11 20:17:32 +0100

    create DiscountContext

---
+++ src/Services/Discount/Discount.API/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.API.Models;
+
+namespace Discount.API
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
--- src/Services/Discount/Discount.API/GlobalUsings.cs
+++ src/Services/Discount/Discount.API/GlobalUsings.cs
@@ -3,6 +3,7 @@
 global using Grpc.Core;
 
 global using Microsoft.AspNetCore.Builder;
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 
 global using BuildingBlocks.Consts;
