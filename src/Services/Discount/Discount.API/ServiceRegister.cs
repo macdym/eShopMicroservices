341de697586d7c84c68740ac360f6fc5e277d060
JACEK SMART <maciej.dyminski02@gmail.com>
2025-02-15 21:25:58 +0100

    add ef tools, ef desing, DiscountContext as sqllite db

--- src/Services/Discount/Discount.API/ServiceRegister.cs
+++ src/Services/Discount/Discount.API/ServiceRegister.cs
@@ -7,6 +7,11 @@
             var assemblyMarker = typeof(Program).Assembly;
             var connectionString = builder.Configuration.GetConnectionString(ServiceRegisterConst.DATABASE);
 
+            builder.Services.AddDbContext<DiscountContext>(opts =>
+            {
+                opts.UseSqlite(connectionString);
+            });
+
             builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
             return builder;
